951ae1a095fdda01b9b6b5e23dcf88a263d88080
Altamiro Figueiredo <altamironeto03@hotmail.com>
2022-06-08 15:47:45 -0300

    Project Created

---
+++ src/main/java/com/altamirofigueiredo/projetospringboot1/Projetospringboot1Application.java
@@ -0,0 +1,13 @@
+package com.altamirofigueiredo.projetospringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetospringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetospringboot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/altamirofigueiredo/projetospringboot1/Projetospringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altamirofigueiredo.projetospringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetospringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

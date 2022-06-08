5873776d55d75126a52080800f61923f88195d1d
Altamiro Figueiredo <altamironeto03@hotmail.com>
2022-06-08 18:16:25 -0300

    H2-DB

--- src/main/java/com/altamirofigueiredo/projetospringboot1/resources/UsuarioResources.java
+++ src/main/java/com/altamirofigueiredo/projetospringboot1/resources/UsuarioResources.java
@@ -1,6 +1,6 @@
 package com.altamirofigueiredo.projetospringboot1.resources;
 
-import com.altamirofigueiredo.projetospringboot1.entities.User;
+import com.altamirofigueiredo.projetospringboot1.entities.Usuario;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResources {
+public class UsuarioResources {
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "666666", "12345" );
+    public ResponseEntity<Usuario> findAll(){
+        Usuario u = new Usuario(1L, "Maria", "maria@gmail.com", "666666", "12345" );
         return ResponseEntity.ok().body(u);
     }
 }

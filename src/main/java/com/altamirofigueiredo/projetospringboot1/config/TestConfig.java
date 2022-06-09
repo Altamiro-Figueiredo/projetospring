8426ec50ae2a3844e67770d02d72b61dd48f0a35
Altamiro Figueiredo <altamironeto03@hotmail.com>
2022-06-09 20:03:21 -0300

    Category

--- src/main/java/com/altamirofigueiredo/projetospringboot1/config/TestConfig.java
+++ src/main/java/com/altamirofigueiredo/projetospringboot1/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.altamirofigueiredo.projetospringboot1.config;
 
+import com.altamirofigueiredo.projetospringboot1.entities.Category;
 import com.altamirofigueiredo.projetospringboot1.entities.Order;
 import com.altamirofigueiredo.projetospringboot1.entities.Usuario;
 import com.altamirofigueiredo.projetospringboot1.entities.enums.OrderStatus;
+import com.altamirofigueiredo.projetospringboot1.repositories.CategoryRepository;
 import com.altamirofigueiredo.projetospringboot1.repositories.OrderRepository;
 import com.altamirofigueiredo.projetospringboot1.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +26,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
--- src/main/java/com/altamirofigueiredo/projetospringboot1/repositories/CategoryRepository.java
+++ src/main/java/com/altamirofigueiredo/projetospringboot1/repositories/CategoryRepository.java
@@ -1,11 +1,10 @@
 package com.altamirofigueiredo.projetospringboot1.repositories;
 
-import com.altamirofigueiredo.projetospringboot1.entities.Order;
-
+import com.altamirofigueiredo.projetospringboot1.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }

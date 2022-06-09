9be364533d726d659f05e40dbf7f1c7a1c3b191a
Altamiro Figueiredo <altamironeto03@hotmail.com>
2022-06-09 20:41:15 -0300

    Association Join Table

--- src/main/java/com/altamirofigueiredo/projetospringboot1/config/TestConfig.java
+++ src/main/java/com/altamirofigueiredo/projetospringboot1/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.altamirofigueiredo.projetospringboot1.config;
 
 import com.altamirofigueiredo.projetospringboot1.entities.Category;
 import com.altamirofigueiredo.projetospringboot1.entities.Order;
+import com.altamirofigueiredo.projetospringboot1.entities.Product;
 import com.altamirofigueiredo.projetospringboot1.entities.Usuario;
 import com.altamirofigueiredo.projetospringboot1.entities.enums.OrderStatus;
 import com.altamirofigueiredo.projetospringboot1.repositories.CategoryRepository;
 import com.altamirofigueiredo.projetospringboot1.repositories.OrderRepository;
+import com.altamirofigueiredo.projetospringboot1.repositories.ProductRepository;
 import com.altamirofigueiredo.projetospringboot1.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -29,6 +31,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -36,7 +41,23 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3 , p4, p5));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3 , p4, p5));
 
         Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
